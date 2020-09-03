6729133b909fb2cacd3cb526e60d038c763c61b1
Camilo Mazo <camilomazo99@gmail.com>
2020-09-03 11:15:08 -0500

    Mi primer commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//Operaciones aritmeticas
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y,suma=0,resta=0,multiplicacion=0,division=0;
+
+    cout<<"ingrese un numerito porfavor "; cin>>x;
+    cout<<"ingrese otro numero porfavor "; cin>>y;
+
+    suma=x+y;resta=x-y;multiplicacion=x*y;division=x/y;
+
+    cout<<"La suma de los numeros es "<<suma<<endl;
+    cout<<"La resta de los numeros es "<<resta<<endl;
+    cout<<"La multiplicacion de los numeros es "<<multiplicacion<<endl;
+    cout<<"La division de los numeros  es"<<division<<endl;
+
+    return 0;
+}
